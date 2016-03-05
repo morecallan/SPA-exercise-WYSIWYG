7806b48b01c5fbffc0ce5056050fdf3b01b23ced
Callan Morrison <callan.l.morrison@gmail.com>
2016-03-05 12:08:17 -0600

    completed task 1

--- wysiwyg.js
+++ wysiwyg.js
@@ -1,32 +1,37 @@
-{
-  title: "Samurai",
-  name: "Tomoe Gozen",
-  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg"
-  lifespan: {
-    birth: 1747,
-    death: 1797
-  }
-}
+var famousPeople = [
 
-{
-  title: "Samurai",
-  name: "Tomoe Gozen",
-  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg"
-  lifespan: {
-    birth: 1747,
-    death: 1797
-  }
-}
+    {
+      title: "Leprechaun",
+      name: "Lucky",
+      bio: "He was briefly replaced by a forgetful wizard name Waldo (Waldo the Wizard) in 1975.",
+      image: "imgs/Lucky.jpg",
+      lifespan: {
+        birth: 1963,
+        death: "Living"
+      }
+    },
 
-{
-  title: "Samurai",
-  name: "Tomoe Gozen",
-  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg"
-  lifespan: {
-    birth: 1747,
-    death: 1797
-  }
-}
+    {
+      title: "Tiger",
+      name: "Tony",
+      bio: "In 1974, where he was deemed 'Tiger of the Year' in an advertising theme taken from the Chinese Lunar Calendar.",
+      image: "imgs/Tony.jpg",
+      lifespan: {
+        birth: 1951,
+        death: "Living"
+      }
+    },
+
+    {
+      title: "Cap'n",
+      name: "Horatio Magellan Crunch",
+      bio: "Captains a ship called the Guppy, and was born on Crunch Island in the Sea of Milk – a magical place with talking trees, crazy creatures and a whole mountain made out of Cap’n Crunch cereal.",
+      image: "imgs/Capn.jpg",
+      lifespan: {
+        birth: 1963,
+        death: "Living"
+
+      }
+    }
+
+]
