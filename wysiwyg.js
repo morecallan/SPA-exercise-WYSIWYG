f05296d226231d8eb7f96c0108048a8cc2e037c9
Callan Morrison <callan.l.morrison@gmail.com>
2016-03-05 12:46:01 -0600

    completed task 4

--- wysiwyg.js
+++ wysiwyg.js
@@ -1,10 +1,12 @@
+var famourPeepsDiv = document.getElementById("famousPeeps");
+
 var famousPeople = [
 
     {
       title: "Leprechaun",
       name: "Lucky",
       bio: "He was briefly replaced by a forgetful wizard name Waldo (Waldo the Wizard) in 1975.",
-      image: "imgs/Lucky.jpg",
+      image: "<img src=imgs/Lucky.jpg>",
       lifespan: {
         birth: 1963,
         death: "Living"
@@ -15,7 +17,7 @@ var famousPeople = [
       title: "Tiger",
       name: "Tony",
       bio: "In 1974, where he was deemed 'Tiger of the Year' in an advertising theme taken from the Chinese Lunar Calendar.",
-      image: "imgs/Tony.jpg",
+      image: "<img src=imgs/Tony.jpg>",
       lifespan: {
         birth: 1951,
         death: "Living"
@@ -26,12 +28,22 @@ var famousPeople = [
       title: "Cap'n",
       name: "Horatio Magellan Crunch",
       bio: "Captains a ship called the Guppy, and was born on Crunch Island in the Sea of Milk – a magical place with talking trees, crazy creatures and a whole mountain made out of Cap’n Crunch cereal.",
-      image: "imgs/Capn.jpg",
+      image: "<img src=imgs/Capn.jpg>",
       lifespan: {
         birth: 1963,
         death: "Living"
 
       }
     }
-
 ]
+
+//Takes famousPeople array, loops through and then outputs it to DOM to match specs
+function addFamousPeepsToDOM(famousPeople){
+    var buildString = "";
+    for (var i = 0; i < famousPeople.length; i++) {
+        buildString += "<person> <header>" + famousPeople[i].title + ": " + famousPeople[i].name + "</header> <section>" + famousPeople[i].bio + famousPeople[i].image + "</section> <footer>" + famousPeople[i].lifespan.birth + " - " + famousPeople[i].lifespan.death + "</footer> </person>";  
+    }
+    famousPeeps.innerHTML += buildString
+}
+
+addFamousPeepsToDOM(famousPeople)
